4ee27ba870eac6a04cfd5ef8e8606351d8c00d70
Yash Somani <101785058+YashSomani18@users.noreply.github.com>
2023-02-02 22:13:12 +0530

    Add files via upload

---
+++ model/User.js
@@ -0,0 +1,28 @@
+import mongoose, { trusted } from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: trusted,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlegth: 6,
+  },
+  blogs: [
+    { type: mongoose.Types.ObjectId, 
+        ref: "Blog", 
+        required: true }
+    ],
+});
+
+export default mongoose.model("User", userSchema);
+//users
